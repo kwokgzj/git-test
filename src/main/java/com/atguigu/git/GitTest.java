19c3945203ff31c813524e9c8169ba278e336e77
KwokStar <1483115728@qq.com>
2022-05-20 18:52:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Kwok
+ * @create 2022-05-20-18:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
